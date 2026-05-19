b9200a009622d5311808ec0169cc902cc369b919
rmkane <rmkane@proton.me>
2026-05-19 19:37:54 -0400

    Add project A scaffolding

---
+++ src/main/java/org/acme/demo/ProjectA.java
@@ -0,0 +1,11 @@
+package org.acme.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectA {
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectA.class, args);
+    }
+}
